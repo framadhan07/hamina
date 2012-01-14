acfe1007aabacb587b4dfedee677c21c71582743
Fajar Rizki <fajarrizki7@gmail.com>
2012-01-14 07:46:47 +0700

    add project rails to hamina

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hamina2::Application.config.secret_token = '9ce7f303e2119e0b081af4e7e51e2493c93758ea77c61190321c9569097cb328f53176065e407c13d1b9d3f5a974fd9046881227f068b3729a48c610efa8c81b'
